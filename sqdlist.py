b000c190f3961c1b531bc7ffb13452b8264e7730
cnc99 <61551537+cnc99@users.noreply.github.com>
2020-03-05 14:14:42 +0000

    Update sqdlist.py

--- sqdlist.py
+++ sqdlist.py
@@ -1,9 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Mar  3 14:37:06 2020
 
-@author: catar
-"""
+
 
 def sqdlist(l):
     i=0
@@ -11,3 +7,33 @@ def sqdlist(l):
         l[i]=(l[i])**2
         i=i+1
     return l
+
+#trace of a matrix
+def trace(m):
+    if len(m)!= len(m[0]):
+        print('matrix is non-square')
+    else:
+        i=0
+        a=0
+        while i<len(m[0]):
+            a=a+m[i][i]
+            i=i+1
+        return a
+
+#create identity matrix
+def id(k):
+   i=0
+   id=[]
+   while i<k:
+       j=0
+       row=[]
+       while j<k:
+           if j==i:
+               row=row+[1]
+           else:
+               row=row+[0]
+           j=j+1
+       id=id+[row]
+       i=i+1
+   return id
+
